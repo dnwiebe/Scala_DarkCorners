280410e5df5c7565a1d16956547e15fed3d5fbcc
Dan Wiebe <dnwiebe@gmail.com>
2017-05-07 09:49:43 -0400

    XML changes

--- src/test/scala/cse/darkcorners/J_ScalaXML.scala
+++ src/test/scala/cse/darkcorners/J_ScalaXML.scala
@@ -13,15 +13,15 @@ class J_ScalaXML extends path.FunSpec {
     describe ("then XML becomes part of the syntax of Scala.") {
       val document =
         <parent>
-          <child ordinal="1" hair="blonde">Brenda</child>
-          <child ordinal="2" hair="brunette">Taryn</child>
+          <child age="7" hair="blonde">Sara</child>
+          <child age="5" hair="brunette">Amanda</child>
         </parent>
 
-      it ("You can .toString it, even if the result is a little wonky") {
+      it ("You can .toString it, even if the formatting is a little wonky") {
         assert (document.toString ===
                   """<parent>
-            |          <child ordinal="1" hair="blonde">Brenda</child>
-            |          <child ordinal="2" hair="brunette">Taryn</child>
+            |          <child age="7" hair="blonde">Sara</child>
+            |          <child age="5" hair="brunette">Amanda</child>
             |        </parent>""".stripMargin
         )
       }
@@ -29,8 +29,8 @@ class J_ScalaXML extends path.FunSpec {
       it ("You can use a jackass form of XPath to query it") {
         val children = document \ "child"
         assert (children.toList === List (
-          <child hair="blonde" ordinal="1">Brenda</child>,
-          <child hair="brunette" ordinal="2">Taryn</child>
+          <child hair="blonde" age="7">Sara</child>,
+          <child hair="brunette" age="5">Amanda</child>
         ))
       }
 
@@ -40,25 +40,30 @@ class J_ScalaXML extends path.FunSpec {
         assert (children (1) \@ "freckles" === "")
       }
 
+      it ("You can get node text") {
+        val children = document \ "child"
+        assert (children (1).text === "Amanda")
+      }
+
       it ("You can add nodes") {
         val children = document \ "child"
-        val moreChildren = children :+ <child ordinal="3" hair="redhead">Betty</child>
+        val moreChildren = children :+ <child age="3" hair="redhead">Betty</child>
 
         assert (moreChildren.toList === List (
-          <child hair="blonde" ordinal="1">Brenda</child>,
-          <child hair="brunette" ordinal="2">Taryn</child>,
-          <child hair="redhead" ordinal="3">Betty</child>
+          <child hair="blonde" age="7">Sara</child>,
+          <child hair="brunette" age="5">Amanda</child>,
+          <child hair="redhead" age="3">Betty</child>
         ))
       }
 
       it ("You can use all the standard collection methods and dynamically create nodes") {
         val modifiedChildren = (document \ "child").map {child =>
-          <child ordinal={child \@ "ordinal"} hair={child \@ "hair"} sex="female">{child text}</child>
+          <child age={child \@ "age"} hair={child \@ "hair"} sex="female">{child text}</child>
         }
 
         assert (modifiedChildren.toList === List (
-          <child hair="blonde" ordinal="1" sex="female">Brenda</child>,
-          <child hair="brunette" ordinal="2" sex="female">Taryn</child>
+          <child hair="blonde" age="7" sex="female">Sara</child>,
+          <child hair="brunette" age="5" sex="female">Amanda</child>
         ))
       }
     }
