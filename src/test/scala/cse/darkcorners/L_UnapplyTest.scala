f59d95c3d63538bd9e1bf4e60cb3ff5f2c837be7
Dan Wiebe <dnwiebe@gmail.com>
2017-05-06 21:56:16 -0400

    Added some code to UnapplyTest

--- src/test/scala/cse/darkcorners/L_UnapplyTest.scala
+++ src/test/scala/cse/darkcorners/L_UnapplyTest.scala
@@ -56,6 +56,14 @@ class L_UnapplyTest extends path.FunSpec {
       }
       val subject = new Car ("Ford", "Focus", 2013, true)
 
+      it ("you can extract it with val and get the values out") {
+        val CarAll (make, model, year, electric) = subject
+        assert (make === subject.make)
+        assert (model === subject.model)
+        assert (year === subject.year)
+        assert (electric === subject.electric)
+      }
+
       it ("you can extract it with a match statement and get the values out") {
         subject match {
           case CarAll (make, model, year, electric) => {
@@ -83,7 +91,14 @@ class L_UnapplyTest extends path.FunSpec {
       val gas = new Car ("Ford", "Focus", 2013, false)
       val electric = new Car ("Toyota", "Prius", 2015, true)
 
-      it ("you can extract the non-electric and get out the values") {
+      it ("you can extract the non-electric with val and get out the values") {
+        val CarAllNonElectric (make, model, year) = gas
+        assert (make === gas.make)
+        assert (model === gas.model)
+        assert (year === gas.year)
+      }
+
+      it ("you can extract the non-electric with a match statement and get out the values") {
         gas match {
           case CarAllNonElectric (make, model, year) => {
             assert (make === gas.make)
@@ -94,7 +109,16 @@ class L_UnapplyTest extends path.FunSpec {
         }
       }
 
-      it ("you cannot extract the electric") {
+      it ("you cannot extract the electric with a val") {
+        try {
+          val CarAllNonElectric (_, _, _) = electric
+        }
+        catch {
+          case e: MatchError => // success
+        }
+      }
+
+      it ("you cannot extract the electric with a match statement") {
         electric match {
           case CarAllNonElectric (_, _, _) => fail ()
           case _ => // succeed
@@ -131,16 +155,30 @@ class L_UnapplyTest extends path.FunSpec {
       val gas = new Car ("Ford", "Focus", 2013, false)
       val electric = new Car ("Toyota", "Prius", 2015, true)
 
-      it ("the gas car doesn't match") {
+      it ("the gas car doesn't match in a val") {
+        try {
+          val CarMake (_) = gas
+        }
+        catch {
+          case e: MatchError => // success
+        }
+      }
+
+      it ("the gas car doesn't match in a match") {
         gas match {
           case CarMake (_: String) => fail ()
           case _ => // succeed
         }
       }
 
-      it ("the electric car does match") {
+      it ("the electric car does match in a val") {
+        val CarMake (make) = electric
+        assert (make === electric.make)
+      }
+
+      it ("the electric car does match in a match") {
         electric match {
-          case CarMake (make: String) => assert (make === "Toyota")
+          case CarMake (make: String) => assert (make === electric.make)
           case _ => fail ()
         }
       }
